e7dcf29d8658dc0549d1cfb8cb33c3a23f4c7936
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-03-27 14:07:39 +0200

    removed unused js

--- collective/portlet/mybookmarks/browser/javascripts/setHiddenStructure.js
+++
@@ -1,9 +0,0 @@
-/*
- * Super-trick file... we apply (only with JS enabled) a special additioal CSS class that hide
- * our images, in this way we do not get bad effects on page load.
- * 
- * We use JS, so not js-able browsers will not see this class, and Javascript enabled ones
- * will remove this class from portlet when images are ready.
- */
-
-document.write('<style type="text/css">#externalBookmarkForm {display:none}</style>');
